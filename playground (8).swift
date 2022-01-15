f57fd79b62d05d49018392ad2c20dede80d1dc81
sumitPachlaniya <97600179+sumitPachlaniya@users.noreply.github.com>
2022-01-15 07:09:45 -0800

    Add files via upload

---
+++ playground (8).swift
@@ -0,0 +1,13 @@
+func leapYr(n:Int) {
+    if n % 4 == 0{
+if n%100==0 && n%400==0{
+print("\(n) - a Leap Yr")
+}else{
+print("\(n) - a Leap Yr")
+}
+}else{
+print("\(n) - NOT a Leap Yr")
+}
+}
+leapYr(n: 2026)
+© 2022 GitHub, Inc.
